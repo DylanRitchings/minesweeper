e0282844469f6b808d01fef350284333f95e9862
DylanRitchings <dylanritchings1998@gmail.com>
2021-08-19 16:54:24 +0100

    started filling numbers

--- minesweeper.py
+++ minesweeper.py
@@ -1,7 +1,7 @@
 from random import randint
 
 boardSize = 8
-mineAmount = 7
+mineAmount = 15
 
 
 def createBoard(size):
@@ -9,22 +9,46 @@ def createBoard(size):
     for n in range(size):
         board.append([])
         for x in range(size):
-            board[n].append("_")
+             board[n].append("_")
     return board
 
 def printBoard(board):
     for row in board:
         print(row)
         
-def placeMines(amount,board):
+def createMines(amount,board):
+    mines=[]
     for n in range(amount):
         x = randint(0,len(board)-1)
         y = randint(0,len(board)-1)
-        board[x][y] = "M"
-    
+        mines.append([x,y])
+        board[y][x] = "M"
+    return(mines)
+
+def placeNumber(board,x,y):
+    if x>=0 and y>=0 and x<boardSize and y<boardSize:
+        current = board[y][x]
+        if current!="M":
+            if current.isdigit():
+                board[y][x] = str(int(current)+1)
+            else:
+                board[y][x] = "1"
+
+def placeNumbers(board,mines):
+    for mine in mines:
+        x = mine[0]
+        y = mine[1]
+        for x2 in range(x-1,x+2):
+            for y2 in range(y-1,y+2):
+                placeNumber(board,x2,y2)
+
+            
+        
+        
 def main():
     board = createBoard(boardSize)
-    placeMines(mineAmount,board)
+    mines = createMines(mineAmount,board)
+    placeNumbers(board,mines)
     printBoard(board)
     
 if __name__ == "__main__":
